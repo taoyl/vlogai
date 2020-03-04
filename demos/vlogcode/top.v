359db88e6b4e8976b71419f375eefe77a92b919d
taoyl <nerotao@foxmail.com>
2020-03-04 23:01:13 +0800

    Update demo vlog code

--- demos/vlogcode/top.v
+++ demos/vlogcode/top.v
@@ -1,29 +1,27 @@
 module top(
      input clk,
      input rst
-             /* vai-auto-port-begin */
-             ,input          CLK
-             ,input          RST_X
-             ,input  [7:0]   IN
-             ,output [7:0]   OUT
-             /* vai-auto-port-end */
+    /* vai-autoport-begin */
+    ,input          CLK_suffix
+    ,input          RST_X_suffix
+    ,input  [7:0]   IN_suffix
+    ,output [7:0]   OUT_suffix
+    /* vai-autoport-end */
 
 );
 
-wire xxx_a;
- // start
-  /* vai-auto-wire-begin */
-  wire         CLK  ;
-  wire         RST  ;
-  wire [9:0]   din  ;
-  wire [3:0]   dinx ;
-  wire [7:0]   LED  ;
-  wire [39:0]  addr1;
-  wire [394:0] addr2;
-  wire [3:0]   strb ;
-  wire         test1;
-  /* vai-auto-wire-end */
-  //end
+  wire xxx_a;
+  /* vai-autowire-begin */
+  wire         prefix_CLK  ;
+  wire         prefix_RST  ;
+  wire [9:0]   prefix_din  ;
+  wire [3:0]   prefix_dinx ;
+  wire [7:0]   prefix_LED  ;
+  wire [39:0]  prefix_addr1;
+  wire [394:0] prefix_addr2;
+  wire [3:0]   prefix_strb ;
+  wire         prefix_test1;
+  /* vai-autowire-end */
 
 
     // example
@@ -31,7 +29,7 @@ wire xxx_a;
          .STEP  (10   )
         ,.LEN   (8'h4f)
         ,.WIDTH (4'h8 )
-    ) u_my_led ( /* vai-auto-inst */
+    ) u_my_led ( /* vai-autoinst */
          .CLK   (prefix_CLK         ) //WI
         ,.RST   (prefix_RST         ) //WI
         ,.din   (prefix_din[9:0]    ) //WI
@@ -43,12 +41,12 @@ wire xxx_a;
         ,.test1 (prefix_test1       ) //WIO
     ); // end of u_my_led
 
-TOP u_top ( /* vai-auto-inst */
-     .CLK   (CLK_suffix     ) //PI
-    ,.RST_X (RST_X_suffix   ) //PI
-    ,.IN    (IN_suffix[7:0] ) //PI
-    ,.OUT   (OUT_suffix[7:0]) //PO
-); // end of u_top
+    TOP u_top ( /* vai-autoinst */
+         .CLK   (CLK_suffix     ) //PI
+        ,.RST_X (RST_X_suffix   ) //PI
+        ,.IN    (IN_suffix[7:0] ) //PI
+        ,.OUT   (OUT_suffix[7:0]) //PO
+    ); // end of u_top
 
  
 endmodule
