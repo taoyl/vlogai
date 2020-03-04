b4102cd77a116b0ac8d7764b0200112f6bdb245b
taoyl <nerotao@foxmail.com>
2020-03-04 15:45:48 +0800

    Update setup file

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
+#!/usr/bin/env python3
+
 import os
 from setuptools import setup, find_packages
 
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
-
 setup(name='vlogai',
       version=read('vlogai/VERSION').splitlines()[0],
-      description=read('README.md'),
+      description='Python package for verilog auto instantiation',
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
       keywords='Verilog, Auto Instantiation, Vim Plugin, Pyverilog',
       author='Yuliang Tao',
       author_email='nerotao@foxmail.com',
-      license='GNU GENERAL PUBLIC LICENSE 3.0',
+      license='GNU GPL 3.0',
       url='https://github.com/taoyl/vlogai',
       packages=find_packages(),
       package_data={'vlogai': ['VERSION'],},
